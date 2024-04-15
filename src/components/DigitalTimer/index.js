3e9733706a8cddd1fa3b19853efb0805be7f3703
Harini <harinisrilekha173@gmail.com>
2024-04-15 11:32:11 +0000

    first commit

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -1,135 +1,137 @@
 // Write your code here
-import {Components} from 'react'
+import {Component} from 'react'
 
 import './index.css'
-const initialState={
-    isTimeRunning: false,
-    setMinutes: 25,
-    setSeconds: 0,
 
+const initialState = {
+  isTimeRunning: false,
+  setMinutes: 25,
+  setSeconds: 0,
 }
 
-class DigitalTimer extends Components {
-
+class DigitalTimer extends Component {
   state = initialState
 
   componentWillUnmount() {
     this.clearTimerInterval()
   }
 
-  clearTimerInterval=() => clearInterval(this.intervalId)
+  clearTimerInterval = () => clearInterval(this.intervalId)
 
   onDecrement = () => {
     const {setMinutes} = this.state
-    if(setMinutes>1){
-        this.setState(prevState => ({setMinutes: prevState.setMinutes - 1,}))
+    if (setMinutes > 1) {
+      this.setState(prevState => ({setMinutes: prevState.setMinutes - 1}))
     }
   }
 
   onIncrement = () => {
-    const {setMinutes} = this.state
     this.setState(prevState => ({setMinutes: prevState.setMinutes + 1}))
   }
 
-   renderButton=()=>{
-       const {setMinutes,setSeconds} = this.state
-       const disabledButton=setSeconds>0
-       return(
-           <div className="device-container">
-           <p className="result-set">Set Timer Limit</p>
-            <div className="increment-container">
-              <button className="button" type="button" disabled={disabledButton}
-              
-              onClick={this.onDecrement}>
-                -
-              </button>
-              <div className="card-para">
-              <p className="card-para">{setMinutes}</p>
-              </div>
-              <button className="button" type="button" 
-               disabled={disabledButton}
-              onClick={this.onIncrement}>
-               +
-              </button>
-              </div>
-       )
-   }
+  renderButton = () => {
+    const {setMinutes} = this.state
+    return (
+      <div className="device-container">
+        <p className="result-set">Set Timer Limit</p>
+        <div className="increment-container">
+          <button className="button1" type="button" onClick={this.onDecrement}>
+            -
+          </button>
+          <p className="card-para">{setMinutes}</p>
+          <button className="button1" type="button" onClick={this.onIncrement}>
+            +
+          </button>
+        </div>
+      </div>
+    )
+  }
 
-   onResetTimer= () => {
-       this.clearTimerInterval()
-       this.setState(initialState)
-   }
+  onResetTimer = () => {
+    this.clearTimerInterval()
+    this.setState(initialState)
+  }
 
-   incrementSeconds=()=>{
-       const{setMinutes,setSeconds}=this.state
-       const isTimerCompleted=setSeconds===setMinutes*60
+  incrementSeconds = () => {
+    const {setMinutes, setSeconds} = this.state
+    const isTimerCompleted = setSeconds === setMinutes * 60
 
-       if(isTimerCompleted){
-           this.clearTimeInterval()
-           this.setState({isTimeRunning:false})
-       }else{
-           this.setState(prevState=>({
-               setSeconds:prevState.setSeconds+1}))
-       }
-   }
-
-  
-onStartOrPause=()=>{
-    const{setMinutes,setSeconds}= this.state
-    const isTimerCompleted= setSeconds===setMinutes*60
-    if(isTimerCompleted){
-        this.setState({setSeconds:0})
-    }if(isTimeRunning){
-        this.clearTimeInterval()
-    }else{
-        this.intervalId=setInterval(this.incrementSeconds,1000)
+    if (isTimerCompleted) {
+      this.clearTimeInterval()
+      this.setState({isTimeRunning: false})
+    } else {
+      this.setState(prevState => ({
+        setSeconds: prevState.setSeconds + 1,
+      }))
     }
-    this.setState(prevState=>({isTimeRunning:prevState.!isTimeRunning}))
-   }
-}
-   
+  }
+
+  onStartOrPause = () => {
+    const {isTimeRunning, setMinutes, setSeconds} = this.state
+    const isTimerCompleted = setSeconds === setMinutes * 60
+    if (isTimerCompleted) {
+      this.setState({setSeconds: 0})
+    }
+    if (isTimeRunning) {
+      this.clearTimeInterval()
+    } else {
+      this.intervalId = setInterval(this.incrementSeconds, 1000)
+    }
+    this.setState(prevState => ({isTimeRunning: !prevState.isTimeRunning}))
+  }
+
   render() {
-    const { isTimeRunning,setMinutes,setSeconds} = this.state
-    const image=isTimeRunning? "https://assets.ccbp.in/frontend/react-js/play-icon-img.png":
-    "https://assets.ccbp.in/frontend/react-js/pause-icon-img.png"
-    const para=isTimeRunning?"Pause-icon": "Play-icon"
+    const {isTimeRunning, setMinutes, setSeconds} = this.state
+    const image = isTimeRunning
+      ? 'https://assets.ccbp.in/frontend/react-js/play-icon-img.png'
+      : 'https://assets.ccbp.in/frontend/react-js/pause-icon-img.png'
+    const para = isTimeRunning ? 'Pause-icon' : 'Play-icon'
+    const method = isTimeRunning ? 'Running' : 'Paused'
     return (
       <div className="bg-container">
         <h1 className="head">Digital Timer</h1>
         <div className="app-container">
           <div className="container">
             <div className="card">
-              <h5 className="head-time"></h5>
-              <p className="para">{}</p>
+              <h5 className="head-time">
+                {setMinutes}:{setSeconds}
+              </h5>
+              <p className="para">{method}</p>
             </div>
           </div>
-          <div className="text-container">
-            <div className="start-pause-container">
-            
-              <button className="button" type="button" onClick={this.onStartOrPause}>
-                <img
-                  src={image}
-                  alt={para}
-                  className="start-img"
-                />
-              </button>
-              <p className="para-set">{isTimeRunning? "Start":"Pause"}</p>
+          <div className="device-container">
+            <div className="text-container">
+              <div className="start-pause-container">
+                <button
+                  className="button"
+                  type="button"
+                  onClick={this.onStartOrPause}
+                >
+                  <img src={image} alt={para} className="start-img" />
+                </button>
+                <p className="para-set">{isTimeRunning ? 'Start' : 'Pause'}</p>
+              </div>
+              <div className="rest-container">
+                <button
+                  className="button"
+                  type="button"
+                  onClick={this.onResetTimer}
+                >
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png"
+                    alt="reset icon"
+                    className="start-img"
+                  />
+                </button>
+                <p className="para-set">Reset</p>
+              </div>
             </div>
-            <div className="rest-container">
-              <button className="button" type="button" onClick={this.onResetTimer}>
-                <img
-                  src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png"
-                  alt="reset icon"
-                  className="start-img"
-                />
-              </button>
-              <p className="para-set">Reset</p>
-            </div>
-            </div>
+            {this.renderButton()}
           </div>
         </div>
+      </div>
     )
   }
 }
 
-exports default DigitalTimer
+export default DigitalTimer
